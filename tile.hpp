108ae0682d5d45eafc8dabe58b4509a54c66d837
Stanislaw Halik <sthalik@misaki.pl>
2022-07-27 15:25:28 +0200

    a

--- tile.hpp
+++ tile.hpp
@@ -23,8 +23,8 @@ struct tile final
     enum class pass_mode : std::uint8_t { pass_blocked, pass_yes, pass_shoot_through, pass_obscured };
     using enum pass_mode;
 
-    tile_image ground_image_;
-    pass_mode passability_ = pass_obscured;
+    tile_image ground_image_, wall_west_, wall_north_;
+    pass_mode passability_ = pass_shoot_through;
 
     explicit operator bool() const noexcept { return !!ground_image_.atlas; }
 };
