dd5c8f0b20304d4f128ed9c6f6e80505e55970b8
Stanislaw Halik <sthalik@misaki.pl>
2022-10-28 12:25:57 +0200

    fix -Werror

--- compat/defs.hpp
+++ compat/defs.hpp
@@ -9,41 +9,41 @@
 #define fm_begin(...) [&]{__VA_ARGS__}()
 
 #define fm_DECLARE_DEPRECATED_COPY_ASSIGNMENT(type)                                 \
-    {{maybe_unused]] [[deprecated]] type(const type&) noexcept = default;           \
-    {{maybe_unused]] [[deprecated]] type& operator=(const type&) noexcept = default
+    [[deprecated, maybe_unused]] type(const type&) noexcept = default;              \
+    [[deprecated, maybe_unused]] type& operator=(const type&) noexcept = default
 
 #define fm_DECLARE_DEFAULT_COPY_ASSIGNMENT(type)                                    \
-    {{maybe_unused]] constexpr type(const type&) noexcept = default;                \
-    {{maybe_unused]] constexpr type& operator=(const type&) noexcept = default
+    [[maybe_unused]] constexpr type(const type&) noexcept = default;                \
+    [[maybe_unused]] constexpr type& operator=(const type&) noexcept = default
 
 #define fm_DECLARE_DEFAULT_COPY_ASSIGNMENT_(type)                                   \
-    {{maybe_unused]] type(const type&) noexcept = default;                          \
-    {{maybe_unused]] type& operator=(const type&) noexcept = default
+    [[maybe_unused]] type(const type&) noexcept = default;                          \
+    [[maybe_unused]] type& operator=(const type&) noexcept = default
 
 #define fm_DECLARE_DELETED_COPY_ASSIGNMENT(type)                                    \
-    {{maybe_unused]] type(const type&) = delete;                                    \
-    {{maybe_unused]] type& operator=(const type&) = delete
+    type(const type&) = delete;                                                     \
+    type& operator=(const type&) = delete
 
 #define fm_DECLARE_DELETED_MOVE_ASSIGNMENT(type)                                    \
-    {{maybe_unused]] type(type&&) = delete;                                         \
-    {{maybe_unused]] type& operator=(type&&) = delete
+    type(type&&) = delete;                                                          \
+    type& operator=(type&&) = delete
 
 #define fm_DECLARE_DEPRECATED_MOVE_ASSIGNMENT(type)                                 \
-    {{maybe_unused]] [[deprecated]] type(type&&) = default;                         \
-    {{maybe_unused]] [[deprecated]] type& operator=(type&&) = default
+    [[deprecated, maybe_unused]] type(type&&) = default;                            \
+    [[deprecated, maybe_unused]] type& operator=(type&&) = default
 
 #define fm_DECLARE_DEFAULT_MOVE_ASSIGNMENT(type)                                    \
-    {{maybe_unused]] constexpr type(type&&) noexcept = default;                     \
-    {{maybe_unused]] constexpr type& operator=(type&&) noexcept = default
+    [[maybe_unused]] constexpr type(type&&) noexcept = default;                     \
+    [[maybe_unused]] constexpr type& operator=(type&&) noexcept = default
 
 #define fm_DECLARE_DEFAULT_MOVE_ASSIGNMENT_(type)                                   \
     [[maybe_unused]] type(type&&) noexcept = default;                               \
     [[maybe_unused]] type& operator=(type&&) noexcept = default
 
 #define fm_DECLARE_DEFAULT_MOVE_COPY_ASSIGNMENTS(type)                              \
-    {{maybe_unused]] fm_DECLARE_DEFAULT_MOVE_ASSIGNMENT(type);                      \
-    {{maybe_unused]] fm_DECLARE_DEFAULT_COPY_ASSIGNMENT(type)
+    [[maybe_unused]] fm_DECLARE_DEFAULT_MOVE_ASSIGNMENT(type);                      \
+    [[maybe_unused]] fm_DECLARE_DEFAULT_COPY_ASSIGNMENT(type)
 
 #define fm_DECLARE_DEFAULT_MOVE_COPY_ASSIGNMENTS_(type)                             \
-    {{maybe_unused]] fm_DECLARE_DEFAULT_MOVE_ASSIGNMENT_(type);                     \
-    {{maybe_unused]] fm_DECLARE_DEFAULT_COPY_ASSIGNMENT_(type)
+    [[maybe_unused]] fm_DECLARE_DEFAULT_MOVE_ASSIGNMENT_(type);                     \
+    [[maybe_unused]] fm_DECLARE_DEFAULT_COPY_ASSIGNMENT_(type)
