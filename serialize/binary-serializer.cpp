56ca299daf0ce278d88a18af6dc6ec528b45161f
Stanislaw Halik <sthalik@misaki.pl>
2022-10-30 07:12:00 +0100

    remove unused code

--- serialize/binary-serializer.cpp
+++ serialize/binary-serializer.cpp
@@ -4,33 +4,6 @@
 
 namespace floormat::Serialize {
 
-#if 0
-template<std::size_t N>
-struct byte_array_iterator final
-{
-    using iterator_category = std::forward_iterator_tag;
-    using difference_type   = std::ptrdiff_t;
-    using value_type        = char;
-    using pointer           = char*;
-    using reference         = char&;
-
-    constexpr byte_array_iterator(char (&buf)[N]) : buf{&buf}, i{0} {}
-    constexpr byte_array_iterator(char (&buf)[N], std::size_t i) : buf{&buf}, i{i} {}
-
-    constexpr reference operator*() const { fm_assert(i < N); return (*buf)[i]; }
-    constexpr pointer operator->() { fm_assert(i < N); return &(*buf)[i]; }
-    constexpr byte_array_iterator<N>& operator++() noexcept { i++; return *this; }
-    constexpr byte_array_iterator<N> operator++(int) noexcept { byte_array_iterator<N> tmp = *this; ++(*this); return tmp; }
-    friend constexpr bool operator==(const byte_array_iterator<N>&& a, const byte_array_iterator<N>&& b) noexcept = default;
-
-private:
-    char (*buf)[N];
-    std::size_t i;
-};
-
-template struct byte_array_iterator<sizeof(double)>;
-#endif
-
 [[maybe_unused]]
 static constexpr bool test1()
 {
