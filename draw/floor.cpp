4b02a2ac8a0ea0f72a2452339fbde4d1eb40c535
Stanislaw Halik <sthalik@misaki.pl>
2022-11-09 12:14:36 +0100

    allow debugging floor draw count per chunk

--- draw/floor.cpp
+++ draw/floor.cpp
@@ -13,6 +13,8 @@ floor_mesh::floor_mesh()
 {
 }
 
+//#define FM_DEBUG_DRAW_COUNT
+
 void floor_mesh::draw(tile_shader& shader, chunk& c)
 {
     auto [mesh_, ids] = c.ensure_ground_mesh();
@@ -20,6 +22,8 @@ void floor_mesh::draw(tile_shader& shader, chunk& c)
     tile_atlas* last_atlas = nullptr;
     std::size_t last_pos = 0;
     GL::MeshView mesh{mesh_};
+
+    [[maybe_unused]] std::size_t draw_count = 0;
 
     const auto do_draw = [&](std::size_t i, tile_atlas* atlas) {
         if (atlas == last_atlas)
@@ -30,6 +34,7 @@ void floor_mesh::draw(tile_shader& shader, chunk& c)
             mesh.setCount((int)(quad_index_count * len));
             mesh.setIndexRange((int)(last_pos*quad_index_count), 0, quad_index_count*TILE_COUNT - 1);
             shader.draw(mesh);
+            draw_count++;
         }
         last_atlas = atlas;
         last_pos = i;
@@ -39,6 +44,11 @@ void floor_mesh::draw(tile_shader& shader, chunk& c)
         if (auto* atlas = c.ground_atlas_at(ids[k]))
             do_draw(k, atlas);
     do_draw(TILE_COUNT, nullptr);
+
+#ifdef FM_DEBUG_DRAW_COUNT
+    if (draw_count)
+        fm_debug("floor draws: %zu", draw_count);
+#endif
 }
 
 } // namespace floormat
