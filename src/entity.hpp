1dd83437e5777f699184452667e7698072fd5cb4
Stanislaw Halik <sthalik@misaki.pl>
2022-11-16 15:26:53 +0100

    entity: pass small trivial types by value

--- src/entity.hpp
+++ src/entity.hpp
@@ -12,8 +12,9 @@ namespace floormat {}
 
 namespace floormat::entities {
 
-template<typename T> struct pass_by_value : std::bool_constant<std::is_fundamental_v<T>> {};
+template<typename T, typename = void> struct pass_by_value : std::bool_constant<std::is_fundamental_v<T>> {};
 template<> struct pass_by_value<StringView> : std::true_type {};
+template<typename T> struct pass_by_value<T, std::enable_if_t<std::is_trivially_copy_constructible_v<T> && sizeof(T) <= sizeof(void*)>> : std::true_type {};
 template<typename T> constexpr inline bool pass_by_value_v = pass_by_value<T>::value;
 
 template<typename T> using const_qualified = std::conditional_t<pass_by_value_v<T>, T, const T&>;
