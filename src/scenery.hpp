3ed15b56aded3a6f58ef98d0cbe17012e28e9430
Stanislaw Halik <sthalik@misaki.pl>
2022-11-20 21:09:03 +0100

    scenery: don't bother with bitfields
    The `scenery_proto` struct is rounded up to a multiple of 8 bytes.

--- src/scenery.hpp
+++ src/scenery.hpp
@@ -6,22 +6,20 @@ namespace floormat {
 
 struct anim_atlas;
 
-enum class rotation : std::uint16_t {
+enum class rotation : std::uint8_t {
     N, NE, E, SE, S, SW, W, NW,
     COUNT,
 };
 
 struct scenery final
 {
-    static constexpr auto NO_FRAME = (1 << 12) - 1;
+    static constexpr auto NO_FRAME = (std::uint16_t)-1;
 
     using frame_t = std::uint16_t;
 
-    rotation r    : 4  = rotation::N;
-    frame_t frame : 12 = NO_FRAME;
+    frame_t frame = NO_FRAME;
+    rotation r = rotation::N;
 };
-
-static_assert(sizeof(scenery) == sizeof(std::uint16_t));
 
 struct scenery_proto final {
     std::shared_ptr<anim_atlas> atlas;
