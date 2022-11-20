dea3a59a9d360a130a6fd42969339b7919d554f0
Stanislaw Halik <sthalik@misaki.pl>
2022-11-20 18:17:59 +0100

    fix StringView ctor not marked as constexpr

--- entity/name-of.hpp
+++ entity/name-of.hpp
@@ -11,8 +11,7 @@ template<typename T>
 static constexpr auto mangled_name() { // NOLINT(bugprone-reserved-identifier)
     using namespace Corrade::Containers;
     using SVF = StringViewFlag;
-    constexpr const char* str = FM_PRETTY_FUNCTION;
-    return StringView { str, Implementation::strlen_(str), SVF::Global|SVF::NullTerminated };
+    return StringView { FM_PRETTY_FUNCTION, SVF::Global|SVF::NullTerminated };
 }
 
 template<typename T> constexpr inline auto name_of = mangled_name<T>();
