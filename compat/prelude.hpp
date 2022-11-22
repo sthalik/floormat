4f458fba80cbcbfecf3fa54284e3004852bbc172
Stanislaw Halik <sthalik@misaki.pl>
2022-11-22 20:54:10 +0100

    compat/prelude: add Vector{2,3,4}uz

--- compat/prelude.hpp
+++ compat/prelude.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
+#ifdef _MSC_VER
+#if defined _WIN64
+typedef unsigned __int64   size_t;
+#else
+typedef unsigned int       size_t;
+#endif
+#else
+typedef __SIZE_TYPE__      size_t;
+#endif
+
 namespace Corrade::Containers::Literals {}
 namespace Corrade::Containers {}
 namespace Corrade::Utility::Path {}
 namespace Corrade::Utility { class Debug; class Error; }
 namespace Magnum::Math::Literals {}
-namespace Magnum {}
+namespace Magnum::Math {
+template<typename T> class Vector2;
+template<typename T> class Vector3;
+template<typename T> class Vector4;
+} // namespace Magnum::Math
+namespace Magnum {
+using Vector2uz = Math::Vector2<::size_t>;
+using Vector3uz = Math::Vector3<::size_t>;
+using Vector4uz = Math::Vector4<::size_t>;
+} // namespace Magnum
+
 namespace floormat {
     using namespace ::Magnum;
     using namespace ::Corrade::Containers;
