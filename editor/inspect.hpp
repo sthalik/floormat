df5291d097abce338870864efd9af6591a2f69d2
Stanislaw Halik <sthalik@misaki.pl>
2023-02-24 11:30:22 +0100

    editor/inspect: move out of arg passed by value

--- editor/inspect.hpp
+++ editor/inspect.hpp
@@ -7,9 +7,9 @@ struct erased_accessor;
 
 template<typename T, typename Enum, Enum As>
 struct field_repr_ final {
-    field_repr_(T x) : value{x} {}
+    field_repr_(T x) : value{std::move(x)} {}
     field_repr_(const field_repr_<T, Enum, As>& other) noexcept = default;
-    field_repr_& operator=(T x) noexcept { value = x; return *this; }
+    field_repr_& operator=(T x) noexcept { value = std::move(x); return *this; }
     field_repr_& operator=(const field_repr_<T, Enum, As>& x) noexcept = default;
     field_repr_& operator*() const noexcept { return value; }
     operator T() const noexcept { return value; }
