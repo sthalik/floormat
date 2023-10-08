8da0d491a0b724dd737491a7144f19c5cc53ba8c
Stanislaw Halik <sthalik@misaki.pl>
2023-10-08 05:07:39 +0200

    aa

--- test/dijkstra.cpp
+++ test/dijkstra.cpp
@@ -1,26 +1,37 @@
 #include "app.hpp"
 #include "path-search.hpp"
 #include <chrono>
+#include <Corrade/Containers/StringView.h>
 
 namespace floormat {
+
+namespace {
+
+template<typename F>
+requires requires (F& fun) { fun(); }
+void bench_run(StringView name, F&& fun)
+{
+    using namespace std::chrono;
+    using clock = high_resolution_clock;
+    const auto t0 = clock::now();
+    fun();
+    for (int i = 0; i < 10; i++)
+        fun();
+    const auto tm = clock::now() - t0;
+    Debug{} << "test" << name << "took" << duration_cast<milliseconds>(tm).count() << "ms.";
+}
+
+} // namespace
 
 void test_app::test_dijkstra()
 {
     auto w = world();
     auto a = astar();
 
-    using namespace std::chrono;
-    using clock = high_resolution_clock;
-
-    const auto t0 =  clock::now();
-
-    for (int i = 0; i < 10; i++)
-    {
-        a.Dijkstra(w, {}, 0, {{0, 0, 0}, {}}, {{1, 1, 0}, {}},
-                       1*TILE_MAX_DIM*iTILE_SIZE2.x());
-    }
-    const auto tm = clock::now() - t0;
-    Debug{} << "test took" << std::chrono::duration_cast<milliseconds>(tm).count() << "ms.";
+    bench_run("Dijkstra", [&] {
+      a.Dijkstra(w, {}, 0, {{0, 0, 0}, {}}, {{1, 1, 0}, {}},
+                 1*TILE_MAX_DIM*iTILE_SIZE2.x());
+    });
 }
 
 } // namespace floormat
