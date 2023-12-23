d93ef1ee0118b52452bd3c0452097433b9ee9e04
Stanislaw Halik <sthalik@misaki.pl>
2023-12-23 13:07:44 +0100

    fix use after free

--- loader/error-tex.cpp
+++ loader/error-tex.cpp
@@ -11,9 +11,14 @@ Trade::ImageData2D loader_impl::make_error_texture(Vector2ui size)
 {
     fm_assert(size.product() != 0);
     constexpr auto magenta = Vector4ub{255, 0, 255, 255};
-    auto array = Array<Vector4ub>{DirectInit, size.product(), magenta};
-    auto img = Trade::ImageData2D{PixelFormat::RGBA8Unorm, Vector2i(size), {},
-                                  std::move(array), {}, {}};
+    auto array = Array<char>{NoInit, 4 * size.product()};
+    auto data = array.data(), end = data + array.size();
+    while (data != end)
+    {
+        *(Vector4ub*)data = magenta;
+        data += 4;
+    }
+    auto img = Trade::ImageData2D{PixelFormat::RGBA8Unorm, Vector2i(size), std::move(array)};
     return img;
 }
 
