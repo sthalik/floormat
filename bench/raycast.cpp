8b9ca5cc3a6d6505183d48216a7c22572592663f
Stanislaw Halik <sthalik@misaki.pl>
2024-02-04 17:29:46 +0100

    wip

---
+++ bench/raycast.cpp
@@ -0,0 +1,20 @@
+#include "src/raycast.hpp"
+#include <benchmark/benchmark.h>
+
+namespace floormat {
+
+namespace {
+
+#warning TODO!
+
+[[maybe_unused]] void Raycast(benchmark::State& state)
+{
+    for (auto _ : state)
+        (void)0;
+}
+
+BENCHMARK(Raycast)->Unit(benchmark::kMicrosecond);
+
+} // namespace
+
+} // namespace floormat
--- test/raycast.cpp
+++ test/raycast.cpp
@@ -1,7 +1,22 @@
 #include "app.hpp"
 #include "src/raycast.hpp"
+#include "src/world.hpp"
 
 namespace floormat {
+
+namespace {
+
+#warning TODO!
+
+void test1()
+{
+    // try reproducing the evil bug in src/raycast.cpp:285:
+    //   c = w.at({last_ch + Vector2i{i - 1, j - 1}});
+    // was incorrectly:
+    //   c = w.at({last_ch - Vector2i{i - 1, j - 1}});
+}
+
+} // namespace
 
 void test_app::test_raycast()
 {
