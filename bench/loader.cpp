47124d3af8a80854e9a35d2c4ec4b783dbe501b0
Stanislaw Halik <sthalik@misaki.pl>
2024-02-05 14:42:57 +0100

    bench: now test loader without its cache
    Previously it only tested loading the .json files.

--- bench/loader.cpp
+++ bench/loader.cpp
@@ -1,4 +1,6 @@
 #include "loader/loader.hpp"
+#include "loader/ground-info.hpp"
+#include "loader/wall-info.hpp"
 #include "serialize/json-helper.hpp"
 #include "serialize/anim.hpp"
 #include <Corrade/Containers/ArrayView.h>
@@ -10,25 +12,48 @@ namespace floormat {
 
 namespace {
 
+constexpr struct {
+    const char* name;
+    Vector2ub size;
+    pass_mode pass = pass_mode::pass;
+} ground_atlases[] = {
+    { "floor-tiles", {44,4} },
+    { "tiles", {8, 5} },
+    { "texel", {2, 2}, pass_mode::blocked },
+    { "metal1", {2,2} },
+};
+
+constexpr const char* wall_atlases[] = {
+    "concrete1", "empty", "test1",
+};
+
+constexpr const char* anim_atlases[] = {
+    "anim/npc-walk",
+    "anim/test-8x8",
+    "scenery/door-close",
+    "scenery/control-panel",
+    "scenery/table",
+};
+
 using nlohmann::json;
+
+void run()
+{
+    for (const auto& str : anim_atlases)
+        (void)loader.get_anim_atlas(str);
+    for (const auto& x : ground_atlases)
+        (void)loader.get_ground_atlas(x.name, x.size, x.pass);
+    for (const auto& name : wall_atlases)
+        (void)loader.get_wall_atlas(name);
+}
 
 void Loader_json(benchmark::State& state)
 {
     loader.destroy();
 
-    // warmup
-    {   for (const auto& x : loader.anim_atlas_list())
-            json_helper::from_json<anim_def>(Path::join(loader.ANIM_PATH, ""_s.join({x, ".json"})));
-        json_helper::from_json<std::vector<json>>(Path::join(loader.VOBJ_PATH, "vobj.json"));
-    }
-
+    run();
     for (auto _ : state)
-        for (int i = 0; i < 10; i++)
-        {
-            for (const auto& x : loader.anim_atlas_list())
-                json_helper::from_json<anim_def>(Path::join(loader.ANIM_PATH, ""_s.join({x, ".json"})));
-            json_helper::from_json<std::vector<json>>(Path::join(loader.VOBJ_PATH, "vobj.json"));
-        }
+        run();
 }
 
 BENCHMARK(Loader_json)->Unit(benchmark::kMillisecond);
