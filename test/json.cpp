5a48da35d255753010ea9f9937050c06982a6d40
Stanislaw Halik <sthalik@misaki.pl>
2024-02-06 14:31:33 +0100

    test/json: remove dead code

--- test/json.cpp
+++ test/json.cpp
@@ -15,27 +15,6 @@
 
 namespace floormat {
 
-#if 0
-static chunk make_test_chunk()
-{
-    auto metal1 = loader.ground_atlas("metal1", {2, 2}, pass_mode::pass),
-         metal2 = loader.ground_atlas("metal2", {2, 2}, pass_mode::blocked),
-         tiles = loader.ground_atlas("tiles", {8, 5}, pass_mode::pass);
-    constexpr auto N = TILE_MAX_DIM;
-    world w;
-    chunk c{w, {}};
-    for (auto [x, k, pt] : c) {
-        x.ground() = { tiles, variant_t(k % tiles->num_tiles()) };
-    }
-    constexpr auto K = N/2;
-    c[{K,   K  }].wall_north() = { metal1, 0 };
-    c[{K,   K  }].wall_west()  = { metal2, 0 };
-    c[{K,   K+1}].wall_north() = { metal1, 0 };
-    c[{K+1, K  }].wall_west()  = { metal2, 0 };
-    return c;
-}
-#endif
-
 void test_app::test_json() // NOLINT(readability-convert-member-functions-to-static)
 {
     fm_assert(Path::exists(Path::join(loader.TEMP_PATH, "CMakeCache.txt")));
