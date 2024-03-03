9ffc768ae012f1cfae39ef7aa93c1ed1dfa52dc7
Stanislaw Halik <sthalik@misaki.pl>
2024-03-03 10:27:09 +0100

    tests?

--- src/timer-ns.cpp
+++ src/timer-ns.cpp
@@ -6,22 +6,7 @@
 
 namespace floormat {
 
-namespace {
 
-constexpr auto MAX = (uint64_t)-1, HALF = MAX/2;
-
-static_assert(MAX - (MAX-0) <= 0);
-static_assert(MAX - (MAX-1) <= 1);
-static_assert(MAX - (MAX-2) <= 2);
-
-static_assert(HALF + HALF + 1 == MAX);;
-static_assert(MAX - HALF <= HALF+1);
-
-//static_assert(MAX - (MAX-1) <= 0); // must fail
-//static_assert(MAX - (MAX-2) <= 1); // must fail
-//static_assert(MAX - HALF <= HALF); // must fail
-
-} // namespace
 
 Debug& operator<<(Debug& dbg, const Ns& box)
 {
--- src/timer-test.cpp
+++ src/timer-test.cpp
@@ -2,6 +2,18 @@
 
 namespace floormat {
 
+namespace {
 
+constexpr auto MAX = 18446744073709551615u, HALF = 9223372036854775808u;
+static_assert(std::is_same_v<uint64_t, std::decay_t<decltype(MAX)>>);
+static_assert(std::is_same_v<uint64_t, std::decay_t<decltype(HALF)>>);
+static_assert(MAX == (uint64_t)-1);
+static_assert(HALF + (HALF - 1) == MAX);
+static_assert(MAX - HALF == HALF - 1);
+static_assert((HALF-1)*2 == MAX - 1);
+
+// todo!
+
+} // namespace
 
 } // namespace floormat
