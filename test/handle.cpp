1250fc680050848d777bbb37aa294ad5cff20436
Stanislaw Halik <sthalik@misaki.pl>
2024-07-13 09:58:02 +0200

    w

--- test/handle.cpp
+++ test/handle.cpp
@@ -40,7 +40,7 @@ void test_page1()
     Page page{start_index};
     auto& item0 = page.allocate(1);
     auto handle0 = item0.handle;
-    fm_assert(item0.handle == Handle{start_index});
+    fm_assert(item0.handle == Handle{start_index, 0});
     fm_assert(item0.object.value == 1);
     page.deallocate(item0.handle);
 
@@ -54,6 +54,9 @@ void test_page1()
     fm_assert(int{item1.handle.counter != handle0.counter} + int{item2.handle.counter != handle0.counter} == 1);
     page.deallocate(item2.handle);
     page.deallocate(item1.handle);
+    fm_assert(item0.object.value == 0);
+    fm_assert(item1.object.value == 0);
+    fm_assert(item2.object.value == 0);
 }
 } // namespace
 
