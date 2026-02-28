d3376021d4015f9235740b238be7008fc36c1e44
Stanislaw Halik <sthalik@misaki.pl>
2026-02-28 09:15:26 +0100

    fix argv[0] harder on win32

--- compat/fix-argv0.cpp
+++ compat/fix-argv0.cpp
@@ -1,6 +1,14 @@
 #include "fix-argv0.hpp"
 #include <cstring>
 
+#ifdef _WIN32
+#include <string.h>
+#define my_strcasecmp _stricmp
+#else
+#include <strings.h>
+#define my_strcasecmp strcasecmp
+#endif
+
 namespace floormat {
 
 char* fix_argv0(char* argv0) noexcept
@@ -8,7 +16,7 @@ char* fix_argv0(char* argv0) noexcept
 #ifdef _WIN32
     if (auto* c = std::strrchr(argv0, '\\'); c && c[1])
     {
-        if (auto* s = std::strrchr(c, '.'); s && !std::strcmp(".exe", s))
+        if (auto* s = std::strrchr(c, '.'); s && !my_strcasecmp(".exe", s))
             *s = '\0';
         return c+1;
     }
