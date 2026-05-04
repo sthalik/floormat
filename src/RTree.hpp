e86d489b1914c40b2992129d98a3625ec3c5afc6
Stanislaw Halik <sthalik@misaki.pl>
2026-05-04 13:55:32 +0200

    src/rtree: merge nushoin/RTree@cf826fb
    fix uninitialized variable

--- src/RTree.hpp
+++ src/RTree.hpp
@@ -751,7 +751,7 @@ int RTREE_QUAL::PickBranch(const Rect* a_rect, Node* a_node)
   ELEMTYPEREAL increase;
   ELEMTYPEREAL bestIncr = (ELEMTYPEREAL)-1;
   ELEMTYPEREAL area;
-  ELEMTYPEREAL bestArea = 0;
+  ELEMTYPEREAL bestArea = (ELEMTYPEREAL)-1;
   int best = 0;
   Rect tempRect;
 
